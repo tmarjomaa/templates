714de378b431df41a759739aac197b421e4ad78b
Tommi Marjomaa <tommi.marjomaa@gmail.com>
2020-07-19 14:41:17 +0300

    Initial commit

---
+++ cse-demo/Initialise-VM.ps1
@@ -0,0 +1,8 @@
+# Set Locale, language etc. 
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"FIRegion.xml`""
+ 
+# Set Timezone
+& tzutil /s "FLE Standard Time"
+ 
+# Set languages/culture
+Set-Culture fi-FI
