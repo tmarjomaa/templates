cdaf21ba95ee8f08697c5668fb8077f5ba6456d0
Tommi Marjomaa <43854362+tmarjomaa@users.noreply.github.com>
2020-07-30 12:49:20 +0300

    add logging to reg creation

--- cse-demo/Initialise-VM-FI.ps1
+++ cse-demo/Initialise-VM-FI.ps1
@@ -8,8 +8,9 @@
 Set-Culture fi-FI
 
 #Set Welcome Screen Input
-New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Force
-New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value "0000040b" -PropertyType String -Force
-New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "4" -Value "0000040b" -PropertyType String -Force
+New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout" -Force | Out-File c:\a.txt
+New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Force | Out-File c:\b.txt
+New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value "0000040b" -PropertyType String -Force | Out-File c:\c.txt
+New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "4" -Value "0000040b" -PropertyType String -Force | Out-File c:\d.txt
 
 Restart-Computer
