ec30c897e04d45a6aab220cca66e8ef1bd37ccad
Tommi Marjomaa <43854362+tmarjomaa@users.noreply.github.com>
2020-07-30 11:50:44 +0300

    troubleshoot registrykeys

--- cse-demo/Initialise-VM-FI.ps1
+++ cse-demo/Initialise-VM-FI.ps1
@@ -9,5 +9,6 @@ Set-Culture fi-FI
 
 #Set Welcome Screen Input
 New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value "0000040b" -PropertyType String -Force
+New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "4" -Value "0000040b" -PropertyType String -Force
 
 Restart-Computer
