ec6b7d708061e0998031a53a718d16cb6267fa55
Tommi Marjomaa <43854362+tmarjomaa@users.noreply.github.com>
2020-07-30 12:17:05 +0300

    test creating reg path before adding keyvalue

--- cse-demo/Initialise-VM-FI.ps1
+++ cse-demo/Initialise-VM-FI.ps1
@@ -8,6 +8,7 @@
 Set-Culture fi-FI
 
 #Set Welcome Screen Input
+New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Force
 New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value "0000040b" -PropertyType String -Force
 New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "4" -Value "0000040b" -PropertyType String -Force
 
