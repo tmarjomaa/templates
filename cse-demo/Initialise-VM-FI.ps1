38da023795b040bc4a2484e4df5d27a1bca5d886
Tommi Marjomaa <43854362+tmarjomaa@users.noreply.github.com>
2020-07-30 11:33:07 +0300

    fix registry propertytype

--- cse-demo/Initialise-VM-FI.ps1
+++ cse-demo/Initialise-VM-FI.ps1
@@ -8,6 +8,6 @@
 Set-Culture fi-FI
 
 #Set Welcome Screen Input
-New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value "0000040b" -PropertyType REG_SZ -Force
+New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value "0000040b" -PropertyType String -Force
 
 Restart-Computer
