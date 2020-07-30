9f8332053a1c063c04b0772971ccbcc5a819ff34
Tommi Marjomaa <43854362+tmarjomaa@users.noreply.github.com>
2020-07-30 13:32:01 +0300

    try another path to see if persistent

--- cse-demo/Initialise-VM-FI.ps1
+++ cse-demo/Initialise-VM-FI.ps1
@@ -8,10 +8,10 @@
 Set-Culture fi-FI
 
 #Set Welcome Screen Input
-New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout" -Force | Out-File c:\a.txt
-New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Force | Out-File c:\b.txt
-New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Substitutes" -Force | Out-File c:\s.txt
-New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value "0000040b" -PropertyType String -Force | Out-File c:\c.txt
-New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "4" -Value "0000040b" -PropertyType String -Force | Out-File c:\d.txt
+New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout2" -Force | Out-File c:\a.txt
+New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout2\Preload" -Force | Out-File c:\b.txt
+New-Item -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout2\Substitutes" -Force | Out-File c:\s.txt
+New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout2\Preload" -Name "1" -Value "0000040b" -PropertyType String -Force | Out-File c:\c.txt
+New-ItemProperty -Path "REGISTRY::HKEY_USERS\.DEFAULT\Keyboard Layout2\Preload" -Name "4" -Value "0000040b" -PropertyType String -Force | Out-File c:\d.txt
 
-# Restart-Computer
+Restart-Computer
